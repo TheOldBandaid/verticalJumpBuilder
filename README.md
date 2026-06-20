90ca07870cd93f10b6143e9465bbcbba2a8529a3
Oldbandaid <lizanick37@gmail.com>
2026-06-21 00:31:33 +0300

    visual-of-block-part1
    + block structure description
    + changing the shape function to a square function (this is more logical, true is a square, false is a triangle)
    + sprite logic update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Vertcal Jump game level Generator 
 Generating an infinite vertical level by gradually increasing the complexity of the created elements, taking into account entry and exit points
+
+## Block
+The smallest unit of level generation, at this stage the shape, visuals and properties of the block are defined
+
+### Visual 
+The development of the visual of the block begins with separating the image of the sides from the internal component in the sprite
+In the «Sprite editor» separate the borders by length and in «Draw Mode» select Tiled. 
+
+*** I found this part a little more difficult to understand than I would have liked, so I'll write down the solution I came up with right away. This is convenient because the size of the borders does not change when the block size is changed, and the triangles are immediately created with empty space inside. ***
+
+
+### Form
+There are two types here - triangles and rectangles
+
+### Properties
--- block.cs
+++ block.cs
@@ -7,32 +7,52 @@ using UnityEngine;
 public class block : MonoBehaviour
 {
     public GameObject blockPrefab;
+    public Sprite sprite;
+    private SpriteRenderer spriteRenderer;
     System.Random ran = new System.Random();
 
-    public PolygonCollider2D Form(bool a) {
+    public PolygonCollider2D Square(bool a) {
         PolygonCollider2D form = GetComponent<PolygonCollider2D>();
 
         float hight = ran.Next(1, 4) * 0.25f;
         float width = ran.Next(1, 4) * 0.25f;
+
         form.points[0] = new Vector2(hight, width);
         form.points[1] = new Vector2(hight, -width);
         form.points[2] = new Vector2(-hight, width);
         form.points[3] = new Vector2(-hight, -width);
-        if (a == false)
+
+        if (!a)
         {
             form.points[ran.Next(0, 4)] = new Vector2(0, 0);
         }
-
         return form;
     }
 
-    void Start()
+    public void Visual(bool a)
     {
-        if (Form(true))
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
+        spriteRenderer.drawMode = SpriteDrawMode.Tiled;
+    }
+
+    public GameObject Form()
+    {
+        bool type = ((ran.Next(0, 2)) != 1);
+        Square(type);
+        if (type)
         {
 
         }
-        else { 
+        else
+        {
+
         }
+        return blockPrefab;
+    }
+
+    private void Start()
+    {
+        
     }
 }
