5736450728a233e75fb04a568dec4bb5fd9cc48d
Oldbandaid <lizanick37@gmail.com>
2026-07-06 22:06:41 +0300

    silly commit
    not really a good changes, but will see

--- block.cs
+++ block.cs
@@ -4,23 +4,24 @@ using System.Drawing;
 using System;
 using UnityEngine;
 using Unity.VisualScripting;
+using UnityEngine.U2D;
+using UnityEditor;
 
 public class block : MonoBehaviour
 {
     private GameObject blockPrefab;
-    // in other script there will be sprite changing, but for now just enter manually
     public Sprite sprite;
     private PolygonCollider2D form;
     private SpriteRenderer spriteRenderer;
-    private SpriteMask mask;
+    
     public System.Random ran = new System.Random();
+    public int corner = -1;
 
     public PolygonCollider2D Rectangle(GameObject obj, bool a) {
         form = obj.AddComponent<PolygonCollider2D>();
 
         float hight = ran.Next(1, 4) * 0.25f;
         float width = ran.Next(1, 4) * 0.25f;
-        form.pathCount = 1;
 
         Vector2[] points = new Vector2[4];
         points[0] = new Vector2(hight, width);
@@ -31,7 +32,8 @@ public class block : MonoBehaviour
 
         if (!a)
         {
-            points[ran.Next(0, 4)] = new Vector2(0, 0);
+            corner = ran.Next(1, 4);
+            points[corner] = new Vector2(0, 0);
         }
         form.SetPath(0, points);
         return form;
@@ -43,30 +45,28 @@ public class block : MonoBehaviour
         spriteRenderer.sprite = sprite;
         spriteRenderer.drawMode = SpriteDrawMode.Tiled;
         spriteRenderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+   
 
-        mask = obj.AddComponent<SpriteMask>();
-        mask.sprite = sprite;
-        mask.bounds = form.bounds;
-        mask.transform.position = new Vector3(0, 0, 0);
+
+
+        if (corner != -1)
+        {
+            corner = corner switch { 0 => 2, 1 => 3, 2 => 0, 3 => 1, _ => -1};
+        }
+        else
+        {
+            corner = ran.Next(1, 4);
+        }
+
     }
 
     private void Start()
     {
         blockPrefab = new GameObject();
         // if type == false, form = triagle else rectangle
-        // bool type = ((ran.Next(0, 2)) != 1);
-        bool type = false;
+        bool type = ((ran.Next(0, 2)) != 1);
 
         Rectangle(blockPrefab, type);
         Visual(blockPrefab);
-        if (type)
-        {
-
-        }
-        else
-        {
-
-        }
-        blockPrefab.transform.position = new Vector3(0, 0, 0);
     }
 }
