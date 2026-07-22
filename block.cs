18542eb371e74b14622ea764be6a85196b7ece44
Oldbandaid <lizanick37@gmail.com>
2026-07-22 14:03:08 +0300

    block-border
    borders of block
    + create polygonCollider
    - mesh is not working

--- block.cs
+++ block.cs
@@ -13,66 +13,52 @@ public class block : MonoBehaviour
     private GameObject blockPrefab;
     public Sprite sprite1, sprite2;
     private PolygonCollider2D form;
+    private PolygonCollider2D borderForm;
     public System.Random ran = new System.Random();
 
-    public PolygonCollider2D Rectangle(GameObject obj, bool a) {
+    public PolygonCollider2D InnerForm(GameObject obj, bool a) {
         form = obj.AddComponent<PolygonCollider2D>();
         float hight = ran.Next(1, 4) * 0.25f;
         float width = ran.Next(1, 4) * 0.25f;
+        Vector2[] points = new Vector2[a ? 4 : 3];
 
-        Vector2[] points = new Vector2[] {
-            new Vector2(width, -hight),
-            new Vector2(-width, -hight),
-            new Vector2(-width, hight),
-            !a ? Vector2.zero : new Vector2(width, hight),
-        };
+        points[0] = new Vector2(width, -hight);
+        points[1] = new Vector2(-width, -hight);
+        points[2] = new Vector2(-width, hight);
+        if (a) { points[3] = new Vector2(width, hight); }
 
         form.points = points;
         return form;
     }
 
-    public PolygonCollider2D Boards(GameObject obj)
+    public PolygonCollider2D BoardsForm(GameObject obj)
     {
-        PolygonCollider2D borderForm = obj.AddComponent<PolygonCollider2D>();
-        int amount = ran.Next(2, form.points.Length);
-        Vector2[] points = new Vector2[amount*2];
-        for (int i = 0; i<amount/2+1; ++i)
-        {
-            points[i] = form.points[i];
-        }
-        float x = form.points[0].x + 0.5f;
-        float y = form.points[3].y + 0.5f;
+        borderForm = obj.AddComponent<PolygonCollider2D>();
+        int amount = ran.Next(2, form.points.Length)*2;
+        Vector2[] points = new Vector2[amount];
 
-        for (int i = amount-1; i > amount/2; i--) {
-            int modX = (i - amount +1)==1 || (i - amount + 1) == 2? -1 :1;
-            int modY = (i-amount+1)/2 == 0 ? -1 : 1;
-            points[i] = new Vector2(x*modX, y*modY);
+        float x = form.points[0].x + 0.2f;
+        float y = form.points[2].y + 0.2f;
+
+        for (int i = 0; i<amount/2; ++i)
+        {
+            int modX = i==0 || i==3 ? 1 : -1;
+            int modY = i<2 ? -1 :1;
+            points[i] = form.points[i];
+            points[amount - 1 - i] = new Vector2(x*modX, y*modY);
         }
+
         borderForm.points = points;
         return borderForm;
 
     }
 
-    public void Visual(GameObject obj, Sprite sprite1, Sprite sprite2)
+    public void Visual(GameObject obj, Sprite sprite, PolygonCollider2D collider)
     {
         MeshRenderer meshRenderer = obj.AddComponent<MeshRenderer>();
         MeshFilter meshFilter = obj.AddComponent<MeshFilter>();
-        Mesh mesh = form.CreateMesh(true, true);
-
-        Material material = meshRenderer.material;
-        material.mainTexture = sprite1.texture;
-        meshRenderer.material = material;
-
-        Vector3[] vertices = mesh.vertices;
-        Vector2[] dots = new Vector2[vertices.Length];
-        for (int i = 0; i<vertices.Length; i++)
-        {
-            float x = vertices[i].x;
-            float y = vertices[i].y;
-            dots[i]= new Vector2(x, y);
-        }
-
-        mesh.uv = dots;
+        Mesh mesh = collider.CreateMesh(false, false);
+        meshRenderer.material.mainTexture = sprite.texture;
         meshFilter.mesh = mesh;
     }
 
@@ -82,8 +68,9 @@ public class block : MonoBehaviour
 
         blockPrefab = new GameObject();
         bool type = ((ran.Next(0, 2)) != 1);
-        Rectangle(blockPrefab, type);
-        Boards(blockPrefab);
-        Visual(blockPrefab, sprite1, sprite2);
+        InnerForm(blockPrefab, type);
+        BoardsForm(blockPrefab);
+        Visual(blockPrefab, sprite1, form);
+        Visual(blockPrefab, sprite2, borderForm);
     }
 }
