7d100ee40d8d50897db2ddd6b093938ab25f3334
Oldbandaid <lizanick37@gmail.com>
2026-09-22 20:32:24 +0300

    feat: commit before merge

--- PlayerScript.cs
+++ PlayerScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public float Velocity;
+    private bool isStopped;
+
+
+
     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTrigger(Collider2D otherCollider)
     {
-        
+        if (otherCollider.CompareTag = "block")
+        {
+
+        }
     }
 }
